4e78cd4c2e1c151777376b7291fcf08695e812f3
Ikraama Sayed <ikraamasayed@gmail.com>
2025-06-17 19:18:08 +0530

    pyramid

---
+++ 19. build pyramid.py
@@ -0,0 +1,11 @@
+def pyramid (n):
+    for i in range(n):
+        for j in range(i,n):
+            print(" ",end="")
+        for j in range(i+1):
+            print("*",end="")
+        # for j in range (i):
+        #     print("*",end="")
+        print("")
+
+pyramid(5)
